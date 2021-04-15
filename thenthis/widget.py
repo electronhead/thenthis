6f23e6cf1f9dfe447fd49cfceb2d310e7d39fab3
electronhead <beaver@electronhead.com>
2021-04-15 12:36:52 -0700

    ui implementation evolution 2

--- thenthis/widget.py
+++ thenthis/widget.py
@@ -1,6 +1,9 @@
 import ipywidgets as widgets
 from pydantic import BaseModel
-from ipywidgets import HBox, VBox, Accordion, AppLayout
+import json
+from datetime import datetime, time, timedelta
+import ipywidgets as widgets
+import whendo.core.util as util
 
 class BaseModelWidget:
     def __init__(self, model:BaseModel):
@@ -11,6 +14,16 @@ class BaseModelWidget:
     def on_change(self, field_name: str):
         def on_value_change(change):
             setattr(self.model, field_name, change["new"])
+        return on_value_change
+
+    def on_time_change(self, field_name: str):
+        def on_value_change(change):
+            setattr(self.model, field_name, str_to_t(change["new"]))
+        return on_value_change
+
+    def on_datetime_change(self, field_name: str):
+        def on_value_change(change):
+            setattr(self.model, field_name, util.str_to_dt(change["new"]))
         return on_value_change
 
     def ui(self):
@@ -34,9 +47,52 @@ class BaseModelWidget:
                         style={"description_width": "initial"},
                     )
                     ui_element.observe(self.on_change(name), names="value")
+                elif type_==bool:
+                    ui_element = widgets.IntText(
+                        value=getattr(self.model, name),
+                        placeholder=str(default) if default else "",
+                        description=name,
+                        style={"description_width": "initial"},
+                    )
+                    ui_element.observe(self.on_change(name), names="value")
+                elif type_==dict:
+                    value_str = json.dumps(getattr(self.model, name))
+                    ui_element = widgets.Text(
+                        value=value_str,
+                        placeholder=str(default) if default else "",
+                        description=name,
+                        style={"description_width": "initial"},
+                    )
+                    ui_element.observe(self.on_change(name), names="value")
+                elif type_==time:
+                    value_str = t_to_str(getattr(self.model, name))
+                    ui_element = widgets.Text(
+                        value=value_str,
+                        placeholder=str(default) if default else "",
+                        description=name,
+                        style={"description_width": "initial"},
+                    )
+                    ui_element.observe(self.on_time_change(name), names="value")
+                elif type_==datetime:
+                    value_str = util.dt_to_str(getattr(self.model, name))
+                    ui_element = widgets.Text(
+                        value=value_str,
+                        placeholder=str(default) if default else "",
+                        description=name,
+                        style={"description_width": "initial"},
+                    )
+                    ui_element.observe(self.on_datetime_change(name), names="value")
                 else:
                     raise TypeError(f"invalid type ({type_})")
                 ui_elements.append(ui_element)
             self.ui_untouched = False
-            self.widget = VBox(children=ui_elements)
+            self.widget = widgets.VBox(children=ui_elements)
         return self.widget
+
+
+def t_to_str(t: time) -> str:
+    return t.isoformat(timespec="seconds")
+
+
+def str_to_t(ts: str) -> time:
+    return time.fromisoformat(ts)
