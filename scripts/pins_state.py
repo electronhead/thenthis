a2c895c5b83f888f5187698987e61fbcf41c584c
gary <gary.beaver@electronhead.com>
2021-06-06 17:13:28 -0700

    fixed some scripts

--- scripts/pins_state.py
+++ scripts/pins_state.py
@@ -3,8 +3,6 @@ GPIO.setmode(GPIO.BCM)
 GPIO.setwarnings(False)
 
 pin=17
-GPIO.setup(pin, GPIO.IN)
 print(f"pin {pin} set to {GPIO.input(pin)}")
 pin=18
-GPIO.setup(pin, GPIO.IN)
 print(f"pin {pin} set to {GPIO.input(pin)}")
